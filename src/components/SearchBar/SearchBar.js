22a24b12bc21845a35be075e765be0735e938629
glauberbrack <glauber@brack.com.br>
2021-01-07 22:46:45 -0300

    feat: input on searchbar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { Segment, Input } from 'semantic-ui-react';
 
-const SearchBar = (...props) => {
-    return (
-        <div><h1>Searchbar</h1></div>
-    )
+class SearchBar extends Component{
+
+    handleSearch = e => {
+        const search = e.target.value
+
+        if(e.keyCode === 13){
+            console.log(search)
+        }
+        
+    }
+
+    render(){
+        return (
+            <div className='search-bar'>
+            <Segment stacked>
+                <Input icon='search' onKeyDown={(e) => this.handleSearch(e)} size='large' placeholder='Type here...'/>
+            </Segment>
+        </div>
+        )
+    }
 }
 
 export default SearchBar
