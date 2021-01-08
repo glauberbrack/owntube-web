8ff56862fe6adcc1305e42b3477c47bf3d2766a6
glauberbrack <glauber@brack.com.br>
2021-01-08 01:06:05 -0300

    feat: showing video list

--- src/components/VideoList/VideoList.js
+++ src/components/VideoList/VideoList.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { List, Image } from 'semantic-ui-react';
+import { connect } from 'react-redux';
 
-const VideoList = (...props) => {
-    return (
-        <div className='video-list'>
-        <h1>VideoList</h1>
-    </div>
-    )
+class VideoList extends Component {
+    renderVideo (video) {
+        const title = video?.snippet?.title
+        return (
+            <List verticalAlign='middle'>
+                <List.Item>
+                    <Image width='100px' height='80px' src={video?.snippet?.thumbnails?.default?.url} />
+                    
+                    <List.Content>
+                        <List.Header>{title.length > 30 ? ((title).substring(0, 30)+'...')  : title}</List.Header>
+                    </List.Content>
+                </List.Item>
+            </List>       
+        )
+    }
+    render() {
+        return (
+            <div className='video-list'>
+                {this.props.videos.map(video => {
+                    return this.renderVideo(video)
+                })}
+            </div>
+        )
+    }
 }
 
-export default VideoList
+const mapStateToProps = (state) => {
+    return {
+        videos: state.search.videos,
+        loading: state.search.loading,
+        error: state.search.error
+    }
+}
+
+export default connect(mapStateToProps, null)(VideoList)
